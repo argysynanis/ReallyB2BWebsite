400aaf63301b90cbb5d1fd65786733a2e1178dff
argysynanis <argysynanis@gmail.com>
2018-11-13 17:13:03 +0000

    Add files via upload

--- ca/js/active.js
+++ ca/js/active.js
@@ -73,14 +73,7 @@
         });
     }
 
-    // :: 3.0 Footer Reveal Active Code
-    if ($.fn.footerReveal) {
-        $('footer').footerReveal({
-            shadow: true,
-            shadowOpacity: 0.3,
-            zIndex: -101
-        });
-    }
+   
 
     // :: 4.0 ScrollUp Active Code
     if ($.fn.scrollUp) {
